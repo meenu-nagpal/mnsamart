cf44e372de46c3f48dae364511f8e32801003299
akashbhagat354 <akashbhagat132@gmail.com>
2021-06-22 21:24:02 +0530

    22 june

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import{Link,NavLink, BrowserRouter as Router, Switch, Route}from 'react-router-dom'
+import store from './Store/store'
+import {Provider} from 'react-redux'
+
+
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+  <App />
+  </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
